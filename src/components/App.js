d09bac18c302fd0afb3cf84833d34433fd223b2b
Pavithephenomenal <pavithravslkanth2001@gmail.com>
2022-03-14 23:50:22 +0530

    Mar 14

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,12 @@
+import "../Styles/App.css";
+import Layout from "./Layout";
+import Home from "./pages/Home";
+
 function App() {
   return (
-    <div className="App">
-      <div>Hello World</div>
-    </div>
+    <Layout>
+      <Home />
+    </Layout>
   );
 }
 
---
+++ src/components/Videos.js
@@ -0,0 +1,17 @@
+import classes from "../Styles/Video.module.css";
+import Video from "./Video";
+
+export default function Videos() {
+  return (
+    <div className={classes.videos}>
+      <Video />
+      <Video />
+      <Video />
+      <Video />
+      <Video />
+      <Video />
+      <Video />
+      <Video />
+    </div>
+  );
+}
